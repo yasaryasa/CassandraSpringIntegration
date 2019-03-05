998097a329e259467a97075b3c5c10b156ba795b
yaser <yaseriztech@gmail.com>
2019-03-05 21:17:20 +0300

    first commit

---
+++ CassandraSpringIntegration/src/main/java/com/yaser/CassandraSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.yaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraSpringIntegrationApplication.class, args);
+	}
+
+}
